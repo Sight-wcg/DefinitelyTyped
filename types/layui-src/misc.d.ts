10bbf08a978bcd41ae5b4eee127bef810844c139
sight <26325820+Sight-wcg@users.noreply.github.com>
2024-12-25 00:22:30 +0800

    update

--- types/layui-src/misc.d.ts
+++ types/layui-src/misc.d.ts
@@ -3697,7 +3697,7 @@ declare namespace Layui {
          * @param [callback]  回调
          * @param [cols]
          */
-        eachCols(id: string, callback?: (...arg: any) => any, cols?: TableColumnOption[][]): void;
+        eachCols(id: string, callback?: AnyFn, cols?: Array<Array<Required<TableColumnOption>>>): void;
 
         /**
          * 导出自定数据到文件
@@ -3808,7 +3808,7 @@ declare namespace Layui {
          * @param [option] 
          * @since 2.8.0
          */
-        setRowChecked(id: string, option?: setRowCheckedOption): void;
+        setRowChecked(id: string, option?: TableSetRowCheckedOption): void;
         /**
          * 获取指定 id 对应的表格实例配置项
          * @param id table 渲染时的 id 属性值
@@ -3837,7 +3837,7 @@ declare namespace Layui {
        * @param expandFlag 要展开或关闭的状态
        * @since 2.8.0
        */
-      beforeExpand(tableId: string, trData: object, expandFlag: boolean): boolean|void;
+      beforeExpand?(tableId: string, trData: object, expandFlag: boolean): boolean|void;
       /**
        * 展开或关闭后的回调函数
        * 
@@ -3846,7 +3846,7 @@ declare namespace Layui {
        * @param expandFlag 要展开或关闭的状态
        * @since 2.8.0
        */
-      onExpand(tableId: string, trData: object, expandFlag: boolean):void
+      onExpand?(tableId: string, trData: object, expandFlag: boolean):void
     }
 
     /**
@@ -3898,6 +3898,14 @@ declare namespace Layui {
        * @since 2.8.0
        */
       autoParam?: string[];
+      /**
+       * 用于处理异步子节点数据的回调函数，该属性优先级高于 async.url 属性
+       * @param trData 当前操作的行数据
+       * @param options 当前表格配置项
+       * @param callback 子节点的渲染函数，参数为子节点数据
+       * @since 2.8.4
+       */
+      format?(trData: object, options: Required<TreeTableOption>, callback: (nodeList: Array<object>) => void): void;
     }
 
     /**
@@ -3917,6 +3925,16 @@ declare namespace Layui {
        * @since 2.8.2
        */
       rootPid?: string | null;
+      /**
+       * 用于设置复选的级联方式。支持以下可选值：
+       * - all : 所有节点联动(默认)
+       * - parent : 仅对父节点联动
+       * - children : 仅对子节点联动
+       * - none 2.8.16+ : 不做任何联动
+       * @default 'all'
+       * @since 2.8.12
+       */
+      cascade?: 'all' | 'parent' | 'children' | 'none';
     }
 
     /**
@@ -3978,6 +3996,12 @@ declare namespace Layui {
        * @since 2.8.0  
        */
       dblClickExpand?: boolean;
+      /**
+       * 是否默认展开全部节点
+       * @default false
+       * @since 2.8.7
+       */
+      expandAllDefault?: boolean;
     }
 
     /**
@@ -4016,11 +4040,17 @@ declare namespace Layui {
        */
       pid?: string;
       /**
+       * 自定义图标的属性名称
+       * @default 'icon'
+       * @since 2.8.4
+       */
+      icon?: string;
+      /**
        * 设置根节点的 pid 属性值
-       * @deprecated 2.8.2 已弃用，请使用 {@link TreeTableTreeOptionData.rootPid|TreeTableTreeOptionData.rootPid}
+       * @deprecated 2.8.2 已移除，请使用 {@link TreeTableTreeOptionData.rootPid|TreeTableTreeOptionData.rootPid}
        * @see {@link TreeTableTreeOptionData.rootPid|TreeTableTreeOptionData.rootPid}
        */
-      rootId: string;
+      rootId?: string;
     }
 
     /**
@@ -4058,17 +4088,76 @@ declare namespace Layui {
       tree?: TreeTableTreeOption;
     }
 
+    interface TreeTableAddNodesOptions{
+      /**
+       * 父节点数据下标
+       * 一般在 tool 事件中，可通过对应数据项中的 `LAY_DATA_INDEX` 特定属性获得
+       */
+      parentIndex: number | string;
+      /**
+       * 节点对应的行下标。若为 -1 表示插入到最后，否则则插入到对应下标。
+       * @default -1
+       */
+      index?: number | string;
+      /**
+       * 新增的节点数据项。若新增的是多个节点，则用数组的形式。若只有一个节点可以是普通对象形式
+       */
+      data: object;
+      /**
+       * 是否聚焦到新增的节点。若存在多个，则聚焦到第一个新增的节点
+       * @default false
+       */
+      focus?: boolean
+    }
+
+    interface TreeTableExpandNodeOptions{
+      /**
+       * 节点数据下标
+       */
+      index: number | string;
+      /**
+       * 设置展开或关闭状态，若为 true 则表示展开；false 则为关闭；null 则表示切换
+       */
+      expandFlag?: boolean | null
+      /**
+       * 子节点是否继承父节点的展开或关闭状态，expandFlag 属性必须为 boolean 型时才有效
+       * @default false
+       */
+      inherit?: boolean;
+      /**
+       * 是否触发事件
+       * @default false
+       */
+      callbackFlag?: boolean
+    }
+
+    interface TreeTableSetRowCheckedOptions{
+      /**
+       * 要设置选中状态的行下标或行数据
+       */
+      index: number | string | object;
+      /**
+       * 选中状态。true 选中；false 取消选中；null 切换。 其中，若为 radio 框，则不支持 null(切换)。
+       */
+      checked?: boolean;
+      /**
+       * 是否触发事件，若为 true，则 checked: false 无效。其对应的事件跟 table 的 radio,checkbox 事件用法一样
+       */
+      callbackFlag?: boolean
+    }
+
     /**
      * treeTable
+     * 
      * @since 2.8.0
      */
-    interface TreeTable extends Table{
+    interface TreeTable extends Omit<Table, 'setRowChecked'>{
         /**
          * treeTable 组件渲染，核心方法
          * @param [option] 基础参数
          * @since 2.8.0
          */
-        render(option?: TreeTableOption): TableRendered;
+        render(option: TreeTableOption): TableRendered;
 
         /**
          * 树表完整重载
@@ -4076,7 +4165,7 @@ declare namespace Layui {
          * @param [option] 基础参数
          * @since 2.8.0
          */
-        reload(id: string, option?: TreeTableOption): void;
+        reload(id: string, option?: Partial<TreeTableOption>): void;
 
 
         /**
@@ -4085,7 +4174,7 @@ declare namespace Layui {
          * @param [option] 基础参数
          * @since 2.8.0
          */
-        reloadData(id: string, option?: TableOption): void;
+        reloadData(id: string, option?: Partial<TreeTableOption>): void;
         /**
          * 获取树表数据
          * 
@@ -4095,7 +4184,7 @@ declare namespace Layui {
          * @param isSimpleData  是否为简单数据，为 true 时返回简单数据结构的数据，否则则为带层级的数据
          * @since 2.8.0
          */
-        getData(id: string, isSimpleData?: boolean):any[];
+        getData(id: string, isSimpleData?: boolean): any[];
         /**
          * 获取树表对应下标的数据
          * 
@@ -4114,60 +4203,80 @@ declare namespace Layui {
          * @param data 更新的数据项，可包含要更新的各种字段
          * @since 2.8.0
          */
-        updateNode(id: string, index: number, data: any):void;
+        updateNode(id: string, index: number, data: object):void;
         /**
          * 删除行记录
-         * @param id 
-         * @param index 
+         * @param id  treeTable 渲染时的 id 属性值
+         * @param index 要删除的节点数据，也可以是节点对应的行下标（ data-index）
          * @since 2.8.0
          */
-        removeNode(id, index):void;
+        removeNode(id: string, index: string | object):void;
         /**
          * 新增行记录
-         * @param id 
-         * @param opts 
+         * @param id treeTable 渲染时的 id 属性值
+         * @param opts 选项
          * @since 2.8.0
          */
-        addNodes(id, opts);
+        addNodes(id: string, opts: TreeTableAddNodesOptions): void;
         /**
          * 展开或关闭节点
-         * @param id 
-         * @param opts 
+         * @param id treeTable 渲染时的 id 属性值
+         * @param opts 选项
          * @since 2.8.0
          */
-        expandNode(id, opts);
+        expandNode(id: string, opts: TreeTableExpandNodeOptions): void;
         /**
          * 展开或关闭全部节点（目前只支持关闭全部）
-         * @param id 
-         * @param expandFlag 
+         * @param id treeTable 渲染时的 id 属性值
+         * @param expandFlag 折叠状态。 true 展开；false 关闭
          * @since 2.8.0
          */
-        expandAll(id, expandFlag);
+        expandAll(id: string, expandFlag: boolean): void;
 
         /**
          * 设置行选中状态
-         * @param id 
+         * @param id treeTable 渲染时的 id 属性值
          * @param opts 
          * @since 2.8.0
          */
-        setRowChecked(id, opts);
+        setRowChecked(id: string, opts: TreeTableSetRowCheckedOptions): void;
 
         /**
          * 全选或取消全选
-         * @param id 
-         * @param checked 
+         * @param id treeTable 渲染时的 id 属性值
+         * @param checked 选中状态。true 选中；false 取消选中；null 复选框模式时的切换。
          * @since 2.8.0
          */
-        checkAllNodes(id, checked);
-
+        checkAllNodes(id: string, checked: boolean): void;
         /**
-         * treeTable 相关事件
-         * @param event 
+         * 
+         * @param id treeTable 渲染时的 id 属性值
+         * @param index 节点对应的行下标，一般可通过 `<tr>` 元素的 `data-index` 属性获得
+         * @since 2.8.4
+         */
+        reloadAsyncNode(id: string, index: string | number): void;
+        /**
+         * 
+         * @param id treeTable 渲染时的 id 属性值
+         * @param dataId 数据项的 id 属性值
+         * @since 2.8.4
+         */
+        getNodeById(id: string, dataId: string): any;
+        /**
+         * 
+         * @param id treeTable 渲染时的 id 属性值
+         * @param filter 过滤函数
+         * @param opts isSingle是否只找到第一个;parentNode在指定在某个父节点下的子节点中搜索
+         * @since 2.8.4
+         */
+        getNodesByFilter(id: string, filter: (item: any) => boolean, opts: {isSingle: boolean; parentNode: object;}): object;
+        /**
+         * 
+         * @param id treeTable 渲染时的 id 属性值
+         * @param [includeHalfCheck=false] 是否包含半选状态的数据(2.8.4)
          * @since 2.8.0
          */
-        on('event(filter)', callback);
-
-
+        checkStatus(id: string, includeHalfCheck?: boolean): {data: []; isAll: boolean};
     }
 
     interface TransferOption {
